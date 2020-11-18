a6252ac2724a84472d7b4f8d46b692026d747ae5
Mike Mason <mason@packet.com>
2020-11-18 10:57:56 -0600

    fix protobuf generation
    Signed-off-by: Mike Mason <mason@packet.com>

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -14,7 +14,20 @@ PROTOS_LOC=api/v1
 OUT_LOC=api/v1
 
 function installDeps {
-    apt update; apt install -y protobuf-compiler
+    if ! which protoc 2>&1 >/dev/null; then
+        echo 'Installing protoc...' >&2
+        if which apt 2>&1 >/dev/null; then
+            apt update; apt install -y protobuf-compiler
+        elif which yum 2>&1 >/dev/null; then
+            yum -y install protobuf-compiler
+        else
+            echo 'Unknown package manager' >&2
+            exit 1
+        fi
+        echo 'Protoc installed' >&2
+    else
+        echo 'Protoc already installed!' >&2
+    fi
     go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.0.1
     go get google.golang.org/grpc@v1.32.0
     go get google.golang.org/protobuf/cmd/protoc-gen-go@v1.25.0
@@ -34,7 +47,6 @@ for elem in $(find ${PROTOS_LOC} -type f -name '*.proto'); do
     protoc -I . -I github.com --go_out=${PWD} --go_opt=module=${REPO} ${elem} || true
     protoc -I . -I github.com --go-grpc_out=${PWD} --go-grpc_opt=module=${REPO} ${elem} || true
 done
-rm ${REPO_BASE}/code
-rm -rf ${REPO} 
+rm -f ${REPO}
 rmdir -p ${REPO_BASE}
 $(go env GOPATH)/bin/goimports -w . || true
