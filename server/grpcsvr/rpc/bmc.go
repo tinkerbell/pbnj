b960f07b4e6bb19963d2cff6f909e45c93c72673
Jacob Weinstock <jakobweinstock@gmail.com>
2020-12-04 09:41:18 -0700

    Fix context cancelled error:
    The context from the request in is causing a
    'context cancelled' error from BMCReset. There's
    potential for this to happen with other oob functions.
    The resolution is the create a new context. Also,
    the task action function doesnt need a goroutine.
    It makes the code more complicated.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- server/grpcsvr/rpc/bmc.go
+++ server/grpcsvr/rpc/bmc.go
@@ -50,8 +50,11 @@ func (b *BmcService) Reset(ctx context.Context, in *v1.ResetRequest) (*v1.ResetR
 			if err != nil {
 				return "", err
 			}
-			// TODO(jacobweinstock) Add task cancellation in taskrunner to honor timeout.
-			taskCtx, _ := context.WithTimeout(context.Background(), defaultTimeout) // nolint
+			taskCtx, cancel := context.WithTimeout(ctx, defaultTimeout)
+			// cant defer this cancel because it cancels the context before the func is run
+			// cant have cancel be _ because go vet complains.
+			// TODO(jacobweinstock): maybe move this context withTimeout into the TaskRunner.Execute function
+			_ = cancel
 			return "", task.BMCReset(taskCtx, in.ResetKind.String())
 		})
 
@@ -78,7 +81,9 @@ func (b *BmcService) CreateUser(ctx context.Context, in *v1.CreateUserRequest) (
 			if err != nil {
 				return "", err
 			}
-			return "", task.CreateUser(ctx)
+			taskCtx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+			_ = cancel
+			return "", task.CreateUser(taskCtx)
 		})
 
 	return &v1.CreateUserResponse{
@@ -104,7 +109,9 @@ func (b *BmcService) UpdateUser(ctx context.Context, in *v1.UpdateUserRequest) (
 			if err != nil {
 				return "", err
 			}
-			return "", task.UpdateUser(ctx)
+			taskCtx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+			_ = cancel
+			return "", task.UpdateUser(taskCtx)
 		})
 
 	return &v1.UpdateUserResponse{
@@ -130,7 +137,9 @@ func (b *BmcService) DeleteUser(ctx context.Context, in *v1.DeleteUserRequest) (
 			if err != nil {
 				return "", err
 			}
-			return "", task.DeleteUser(ctx)
+			taskCtx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+			_ = cancel
+			return "", task.DeleteUser(taskCtx)
 		})
 
 	return &v1.DeleteUserResponse{
