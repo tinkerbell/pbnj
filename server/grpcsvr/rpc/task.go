660fbbafdcd7e64b4c54dee18a33d30ebf1fb1f8
Jacob Weinstock <jakobweinstock@gmail.com>
2021-01-27 17:33:22 -0700

    Simplify logging:
    Simplify logging by only logging a start and end to
    requests. Move the taskID creation up to the RPC methods
    so that the taskID can be included in all logs in a request
    context.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- server/grpcsvr/rpc/task.go
+++ server/grpcsvr/rpc/task.go
@@ -3,7 +3,6 @@ package rpc
 import (
 	"context"
 
-	"github.com/pkg/errors"
 	v1 "github.com/tinkerbell/pbnj/api/v1"
 	"github.com/tinkerbell/pbnj/pkg/logging"
 	"github.com/tinkerbell/pbnj/pkg/task"
@@ -19,14 +18,21 @@ type TaskService struct {
 // Status returns a task record
 func (t *TaskService) Status(ctx context.Context, in *v1.StatusRequest) (*v1.StatusResponse, error) {
 	l := t.Log.GetContextLogger(ctx)
-	l.V(0).Info("getting task record")
-	if err := in.Validate(); err != nil {
-		return nil, errors.Wrap(err, "input arguments are invalid")
-	}
+	l.V(0).Info("start Status request", "taskID", in.TaskId)
 
 	record, err := t.TaskRunner.Status(ctx, in.TaskId)
 	if err != nil {
 		return nil, err
+	}
+	var errMsg *v1.Error
+	if record.Error != nil {
+		errMsg = &v1.Error{
+			Code:    record.Error.Code,
+			Message: record.Error.Message,
+			Details: record.Error.Details,
+		}
+	} else {
+		errMsg = nil
 	}
 	// TODO should i return this as error or just return the error in the response message?
 	/*if record.Error != "" {
@@ -35,14 +41,10 @@ func (t *TaskService) Status(ctx context.Context, in *v1.StatusRequest) (*v1.Sta
 	return &v1.StatusResponse{
 		Id:          record.Id,
 		Description: record.Description,
-		Error: &v1.Error{
-			Code:    record.Error.Code,
-			Message: record.Error.Message,
-			Details: record.Error.Details,
-		},
-		State:    record.State,
-		Result:   record.Result,
-		Complete: record.Complete,
-		Messages: record.Messages,
+		Error:       errMsg,
+		State:       record.State,
+		Result:      record.Result,
+		Complete:    record.Complete,
+		Messages:    record.Messages,
 	}, err
 }
